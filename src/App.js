cbd5f3da5d40c0548258be1720c810feac11e3e4
TasfiaTabassumTasfiaTabassum <tasfia.tabassum.du@gmail.com>
2024-03-15 01:13:37 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskManager from './components/TaskManager';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container d-flex justify-content-center align-items-center">
+      <TaskManager />
+    </div>
+  );
+}
+
+export default App;
